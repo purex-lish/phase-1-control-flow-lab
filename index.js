c2879338d9446d6465a72d2e3460b47a4e3b4253
Purity Wambua <puritywamb@gmail.com>
2024-06-27 20:52:45 +0300

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+  function scuberGreetingForFeet(feet) {
+    if (feet <= 400) {
+      return 'This one is on me!';
+    } else if (feet > 400 && feet < 2000) {
+      return 'That will be twenty bucks.';
+
+    } else if (feet > 2000 && feet < 2500 ) {
+      return 'I will gladly take your thirty bucks.';
+
+    } else if (feet > 2500) {
+      return 'No can do.';
+    }
+  }
+  
+function ternaryCheckCity(city){
+  if (city === "NYC") { 
+     return "Ok, sounds good."; 
+    }
+    else {
+      return "No go.";
+      }
+    }
+
+  
+
+function switchOnCharmFromTip(tip){
+  
+if (tip === "generous") {
+        return "Thank you so much.";
+    } 
+    else if (tip === "not as generous") 
+      {
+        return "Thank you.";
+    } 
+    else {
+        return "Bye.";
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+/*
+
+
+
+ternaryCheckCity()
+5) returns "Ok, sounds good." when the city is NYC
+6) should return "No go." if the destination city is not NYC
+switchOnCharmFromTip()
+7) should return "Thank you so much." if the tip is generous
+8) should return "Thank you." if the tip is not as generous
+9) should return "Bye." if anything else
+
+*/
